c91b7ccf78b2ba5ac58500ebf4a758837f8add20
ketace06 <ismail.omaramin06@gmail.com>
2025-04-29 16:00:00 +0200

    feat: new style and welcome page

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
 import './style.css'
 
-console.log('Hello from typescript')
+document.addEventListener('DOMContentLoaded', () => {
+  const startButton = document.getElementById('start-button') as HTMLButtonElement;
+  const welcomeScreen = document.getElementById('welcome-screen') as HTMLElement;
+  const app = document.getElementById('app') as HTMLElement;
+  
+  function exitMainPage() {
+    
+}
+  
+  if (startButton) {
+    startButton.addEventListener("click", exitMainPage);
+  }
+});
