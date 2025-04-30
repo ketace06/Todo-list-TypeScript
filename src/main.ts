aaeda8edaa6718f15ab00ac2b8069c546b4b17b7
ketace06 <ismail.omaramin06@gmail.com>
2025-04-30 09:00:44 +0200

    feat: Implement style and ts for welcome div and remove when click

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const app = document.getElementById('app') as HTMLElement;
   
   function exitMainPage() {
-    
-}
+    startButton.innerText = "Let's go 🚀";
+    startButton.classList.add('start-button-fade');
+
+    setTimeout(() => {
+      welcomeScreen.classList.add('fade-out');
+    }, 1000);
+    setTimeout(() => {
+      welcomeScreen.remove()
+    }, 2000)
+  }
   
   if (startButton) {
-    startButton.addEventListener("click", exitMainPage);
+    setTimeout(() => {
+      startButton.addEventListener("click", exitMainPage);
+    }, 1500);
   }
+  
 });
